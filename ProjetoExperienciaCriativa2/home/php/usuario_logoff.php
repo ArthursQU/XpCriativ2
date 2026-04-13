f0d3fa75fdc7489bad3694bafc020e1b8c316264
ArthursQU <arthurschmitt206@gmail.com>
2026-04-13 00:10:29 -0300

    Delete ProjetoExperienciaCriativa2 directory

--- ProjetoExperienciaCriativa2/home/php/usuario_logoff.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-
-$retorno = ['status' => 'ok', 'mensagem' => 'Logout efetuado.', 'data' => []];
-
-header("Content-type:application/json;charset:utf-8");
-echo json_encode($retorno);
