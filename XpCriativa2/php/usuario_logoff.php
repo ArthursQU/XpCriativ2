71e1b9d86e0e9bd0deee83d5d02d0803c55c41f3
ArthursQU <arthurschmitt206@gmail.com>
2026-05-12 10:33:54 -0300

    Delete XpCriativa2 directory

--- XpCriativa2/php/usuario_logoff.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-header("Content-type:application/json;charset:utf-8");
-echo json_encode(['status' => 'ok', 'mensagem' => 'Logoff efetuado com sucesso.']);
-?>
